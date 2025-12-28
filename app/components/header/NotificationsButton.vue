9ea8de546b2d0dfb84191e2448bf741ecb2dc1f5
Caio Alfonso <caio.alfonso@cilia.com.br>
2025-12-28 20:32:24 -0300

    feat: create the notification's button component

---
+++ app/components/header/NotificationsButton.vue
@@ -0,0 +1,50 @@
+<template>
+	<OverlayBadge
+		v-if="unread.size"
+		:value="unread.size"
+		size="small"
+		severity="danger"
+	>
+		<Button
+			class="text-2xl text-gray-500"
+			variant="text"
+			rounded
+			@click="toggleNotificationsMenu"
+		>
+			<template #icon>
+				<NuxtIcon name="notifications_active" />
+			</template>
+		</Button>
+	</OverlayBadge>
+
+	<Button
+		v-else
+		class="text-2xl text-gray-500"
+		variant="text"
+		rounded
+		@click="toggleNotificationsMenu"
+	>
+		<template #icon>
+			<NuxtIcon name="notifications" />
+		</template>
+	</Button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+	unread: Set<number>;
+}
+
+interface Emits {
+	(e: 'toggle', event: PointerEvent): any;
+}
+
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>();
+
+const toggleNotificationsMenu = (event: PointerEvent) => {
+	emits('toggle', event);
+};
+</script>
+
+<style scoped></style>
