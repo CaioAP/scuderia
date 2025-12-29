226a20dc62216b3ea16d59eb00c3e009cace756b
Caio Alfonso <caioap25@gmail.com>
2025-12-29 01:33:25 -0300

    feat: create the base Avatar component to handle different Avatar options

---
+++ app/components/BaseAvatar.vue
@@ -0,0 +1,37 @@
+<template>
+	<Avatar
+		v-if="props.image"
+		:image="props.image"
+		:shape="props.shape"
+	/>
+	<Avatar
+		v-else-if="props.icon"
+		:shape="props.shape"
+	>
+		<template #icon>
+			<NuxtIcon :name="props.icon" />
+		</template>
+	</Avatar>
+	<Avatar
+		v-else
+		:label="props.label"
+		:shape="props.shape"
+	/>
+</template>
+
+<script setup lang="ts">
+import type { AvatarProps } from 'primevue';
+
+interface Props {
+	image?: string;
+	icon?: string;
+	label?: string;
+	shape?: 'square' | 'circle';
+}
+
+const props = withDefaults(defineProps<Props & AvatarProps>(), {
+	shape: 'circle',
+});
+</script>
+
+<style scoped lang="scss"></style>
