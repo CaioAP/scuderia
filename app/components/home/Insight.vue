2c49eed4ef1e314df106fd53d351f3471fc12f9c
Caio Alfonso <caioap25@gmail.com>
2025-12-29 01:34:03 -0300

    feat: create the home page's Insight component

---
+++ app/components/home/Insight.vue
@@ -0,0 +1,32 @@
+<template>
+	<NuxtLink
+		class="rounded-lg overflow-hidden"
+		:to="{ name: props.to }"
+	>
+		<div
+			class="w-full h-full px-3 py-2 relative flex flex-col bg-white hover:bg-primary-50"
+		>
+			<div class="absolute inset-0 opacity-10 z-0">
+				<slot name="background" />
+			</div>
+
+			<h2 class="text-lg text-gray-700">{{ props.title }}</h2>
+
+			<slot />
+		</div>
+	</NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { RouteRecordName } from 'vue-router';
+
+interface Props {
+	title: string;
+	to: RouteRecordName;
+	background?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
