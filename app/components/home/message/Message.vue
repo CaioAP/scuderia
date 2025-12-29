e13d858530a02a5965778299864decd7222e43e5
Caio Alfonso <caioap25@gmail.com>
2025-12-29 18:40:05 -0300

    feat: create the home page's message card component

---
+++ app/components/home/message/Message.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="px-4 pt-2 pb-4">
+		<div>
+			<h2 class="text-lg text-gray-700">O que você está pensando hoje? 🤔</h2>
+			<p class="text-sm text-gray-500">
+				Compartilhe com seus colegas uma frase, um sentimento ou algo
+				interessante.
+			</p>
+		</div>
+
+		<HomeMessageForm class="mt-4" />
+	</div>
+</template>
+
+<script setup lang="ts"></script>
